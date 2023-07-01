1da3de4eb0335fe33d28f478dc366179c9970e1d
Jose Berrio <aldarisbm@gmail.com>
2023-07-01 00:49:17 -0400

    clean clean clean

--- main.go
+++ main.go
@@ -17,47 +17,54 @@ func main() {
 
 	for {
 		prompt := getUserInput("Prompt")
-		if prompt == "" {
+		switch prompt {
+		case "":
 			continue
-		}
-		if prompt == "clear" || prompt == "c" {
+		case "q", "quit", "exit":
+			fmt.Println("Exiting...")
+			return
+		case "clear", "c":
 			bot.ClearMessages()
 			fmt.Println("Messages cleared...")
 			continue
+		default:
+			process(ctx, bot, prompt)
 		}
+	}
+}
 
-		req := bot.GetRequest(prompt)
+func process(ctx context.Context, bot *ChatBot, prompt string) {
+	req := bot.GetRequest(prompt)
 
-		stream, err := bot.client.CreateChatCompletionStream(ctx, req)
+	stream, err := bot.client.CreateChatCompletionStream(ctx, req)
+	if err != nil {
+		fmt.Printf("ChatCompletionStream error: %v\n", err)
+	}
+
+	if stream.GetResponse().StatusCode != http.StatusOK {
+		body, err := io.ReadAll(stream.GetResponse().Body)
 		if err != nil {
-			fmt.Printf("ChatCompletionStream error: %v\n", err)
+			fmt.Printf("error reading body: %v\n", err)
+			return
 		}
+		fmt.Printf("non 200 resp: %v\n", string(body))
+	}
+	defer stream.Close()
 
-		if stream.GetResponse().StatusCode != http.StatusOK {
-			body, err := io.ReadAll(stream.GetResponse().Body)
-			if err != nil {
-				fmt.Printf("error reading body: %v\n", err)
-				continue
-			}
-			fmt.Printf("non 200 resp: %v\n", string(body))
+	var lastMessage string
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) && lastMessage != "" {
+			bot.saveMessageToContext(assistant, lastMessage)
+			lastMessage = ""
+			fmt.Println()
+			break
 		}
-		defer stream.Close()
-
-		var lastMessage string
-		for {
-			response, err := stream.Recv()
-			if errors.Is(err, io.EOF) && lastMessage != "" {
-				bot.saveMessageToContext(assistant, lastMessage)
-				lastMessage = ""
-				fmt.Println()
-				break
-			}
-			if err != nil {
-				fmt.Printf("Stream error: %v\n", err)
-			}
-			incomingStr := response.Choices[0].Delta.Content
-			lastMessage += incomingStr
-			fmt.Print(incomingStr)
+		if err != nil {
+			fmt.Printf("Stream error: %v\n", err)
 		}
+		incomingStr := response.Choices[0].Delta.Content
+		lastMessage += incomingStr
+		fmt.Print(incomingStr)
 	}
 }
